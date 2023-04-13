7404ba34bc9e69871b991624392bdd814c375166
Abderafie <chairi.abderafie@student.emi.ac.ma>
2023-04-13 23:59:45 +0200

    ahmed

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -1,6 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Achievements.css"
 export default function Achievements() {
+    const [year, setYear] = useState("2020/2021")
+    const [data, setData] = useState([
+        {
+            year: "2020/2021",
+            achievments: [
+                {
+                    date: "01/2021",
+                    achievement: "First achievement",
+                    data: "First data"
+                },
+                {
+                    date: "02/2021",
+                    achievement: "Second achievement",
+                    data: "Second data"
+                }
+            ]
+        },
+        {
+            year: "2021/2022",
+            achievments: [
+                {
+                    date: "01/2021",
+                    achievement: "First achievement",
+                    data: "ahmed data"
+                },
+                {
+                    date: "02/2021",
+                    achievement: "Second achievement",
+                    data: "abderafie data"
+                }
+            ]
+        },
+        {
+            year: "2022/2023",
+            achievments: [
+                {
+                    date: "01/2021",
+                    achievement: "First achievement",
+                    data: "youssef data"
+                },
+                {
+                    date: "02/2021",
+                    achievement: "Second achievement",
+                    data: "khadija data"
+                }
+            ]
+        }
+    ])
   return (
     <div>
         <h1 style={{textAlign:'center'}}>Achievements</h1>
@@ -8,74 +56,39 @@ export default function Achievements() {
     <div className="ach-container">
     <div>
         <div className="ach-years">
-            <div className="ach-year">
-                2020/2021
-            </div>
-            <div className="ach-year">
-                2021/2022
-            </div>
-            <div className="ach-year">
-                2022/2023
-            </div>
+            {data.map((item, index) => (
+                <div className="ach-year" key={index} onClick={() => setYear(item.year)}>
+                    {item.year}
+                </div>
+            ))}
+
         </div>
     </div>
     <div className="ach1">
         <div className="achs">
-            <div className="ach">
-                <div className="ach-side">
-                    <div className="ach-row">
-                        <div>01/2021</div>
-                        <div className="timeline">
-                        <div className="cercle"></div>
-                        <div className="line"></div>
+            {
+                data.filter(i=>i.year==year)[0].achievments.map((item, index) => (
+                    <div className="ach">
+                        <div className="ach-side">
+                            <div className="ach-row">
+                                <div>{item.date}</div>
+                                <div className="timeline">
+                                    <div className="cercle"></div>
+                                    <div className="line"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ach-cont">
+                            <div className="ach-h">
+                                {item.achievement}
+                            </div>
+                            <div className="ach-txt">
+                                {item.data}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="ach-cont">
-                    <div className="ach-h">
-                        First header
-                    </div>
-                    <div className="ach-txt">
-                    </div>
-                </div>
-            </div>
-            <div className="ach">
-                <div className="ach-side">
-                    <div className="ach-row">
-                        <div>01/2021</div>
-                        <div className="timeline">
-                        <div className="cercle"></div>
-                        <div className="line"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="ach-cont">
-                    <div className="ach-h">
-                        First header
-                    </div>
-                    <div className="ach-txt">
-                    </div>
-                </div>
-            </div>
-            <div className="ach">
-                <div className="ach-side">
-                    <div className="ach-row">
-                        <div>01/2021</div>
-                        <div className="timeline">
-                        <div className="cercle"></div>
-                        <div className="line"></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="ach-cont">
-                    <div className="ach-h">
-                        First header
-                    </div>
-                    <div className="ach-txt">
-                    </div>
-                </div>
-            </div>
-    
+                ))
+            }
         </div>
     </div>
 </div>
--- src/components/Members/Members.tsx
+++ src/components/Members/Members.tsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Members.css"
 import portrait from '../../portrait.png'
 export default function Members() {
-  return (
-    <div>
-    <h1 style={{textAlign:'center',marginTop:70}}>Our Comunity for test</h1>
+    const [members, setMembers] = useState([
+        {
+            name: "Chairi Ahmed",
+            profession: "Developer"
+        },
+        {
+            name: "Jane Doe",
+            profession: "Designer"
+        },
+        {
+            name: "John Doe",
+            profession: "Developer"
+        },
+        {
+            name: "Jane Doe",
+            profession: "Designer"
+        },
+    ]);
+  
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center', marginTop: 70 }}>
+          Our Community for test
+        </h1>
         <div className="mem-list">
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/> 
-            <Member/>
+          {members.map((member, index) => (
+            <Member key={index}  name={member.name} profession={member.profession}/>
+          ))}
         </div>
-    </div>
-  )
-}
+      </div>
+    );
+  }
+  
 
-const Member=(props:any)=>{
-    return(
-    <div className="mem-cont" >
+  const Member = ({ name, profession }: { name: string, profession: string }) => {
+    return (
+      <div className="mem-cont">
         <div className="mem-h">
-            <div className="mem-img">
-                <img src={portrait} height="80" width="80" alt='portrait'/>
-            </div>
-            <div style={{padding:10}}>
-                <div style={{fontWeight: "bold",color: "#091E3E"}}>
-                    Abderafie
-                </div>
-                <div className="job">Profession
-                </div>
-            </div>
+          <div className="mem-img">
+            <img src={portrait} height="80" width="80" alt="portrait" />
+          </div>
+          <div style={{ padding: 10 }}>
+            <div style={{ fontWeight: "bold", color: "#091E3E" }}>{name}</div>
+            <div className="job">{profession}</div>
+          </div>
         </div>
         <div className="hr" color="black"></div>
-    </div>
-    )
-}
+      </div>
+    );
+  };
